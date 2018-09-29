3d801c6e620c9cd12fb6d3e8dff909e24d2f70e1
Quinten <duongquinten@gmail.com>
2018-09-28 21:53:38 -0400

    Finished

--- app/src/main/java/com/example/quinten/lab6/GenerateCharacterService.java
+++ app/src/main/java/com/example/quinten/lab6/GenerateCharacterService.java
@@ -1,10 +1,24 @@
+//Quinten Whitaker
+//Lab 6 - Random Character Generator
+//CS 4322
+//Created on 9-28-18
+
 package com.example.quinten.lab6;
 
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
+
+import java.util.Random;
 
 public class GenerateCharacterService extends Service {
+
+    private boolean isGeneratorOn;
+    private char myRandomChar;
+    private final String TAG = "RandomCharService: ";
+
+
     public GenerateCharacterService() {
     }
 
@@ -12,5 +26,59 @@ public class GenerateCharacterService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Log.i(TAG, "In OnStartCommand Thread ID is "+Thread.currentThread().getId());
+        isGeneratorOn = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startRandomGenerator();
+            }
+        }).start();
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopRandomGenerator();
+        Log.i(TAG, "Service Destroyed");
+    }
+
+    private void stopRandomGenerator()
+    {
+        isGeneratorOn = false;
+    }
+
+    private void startRandomGenerator()
+    {
+        while(isGeneratorOn)
+        {
+            try
+            {
+                Thread.sleep(1000);
+                if(isGeneratorOn)
+                {
+                    Random r = new Random();
+                    char c = (char)(r.nextInt(26) + 'a');
+                    myRandomChar = c;
+                    Log.i(TAG, "Random char is " + myRandomChar);
+                }
+            }
+            catch (InterruptedException e)
+            {
+                Log.i(TAG, "Thread Interrupted");
+            }
+        }
+    }
+
+    private char getRandom()
+    {
+        return myRandomChar;
     }
 }
--- app/src/main/java/com/example/quinten/lab6/MainActivity.java
+++ app/src/main/java/com/example/quinten/lab6/MainActivity.java
@@ -1,13 +1,48 @@
+//Quinten Whitaker
+//Lab 6 - Random Character Generator Service
+//Created on 9-28-18
+//CS 4322
 package com.example.quinten.lab6;
 
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+    Button btn2;
+    Intent serviceIntent;
+    private GenerateCharacterService myService;
+    private ServiceConnection myServiceConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = (Button)findViewById(R.id.startButton);
+        btn2 = (Button)findViewById(R.id.stopButton);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serviceIntent = new Intent(getApplicationContext(), GenerateCharacterService.class);
+                startService(serviceIntent);
+                Toast.makeText(getApplicationContext(), "Service Started", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serviceIntent = new Intent(getApplicationContext(), GenerateCharacterService.class);
+                stopService(serviceIntent);
+                Toast.makeText(getApplicationContext(), "Service Stopped", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
